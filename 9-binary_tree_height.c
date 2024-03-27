d5e9fb81194bbc500a9b86d1969ac7bcf3fdec40
vickram <vickramodero6@gmail.com>
2024-03-27 11:44:55 +0300

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t lh = 0, rh = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	lh = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rh = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
+	if (lh > rh)
+		return (lh);
+	else
+		return (rh);
 }
