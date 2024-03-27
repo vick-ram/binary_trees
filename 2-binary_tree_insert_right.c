d9c606fea4ef994eaa98c71ace5f7e6f060e066a
vick-ram <vickramodero6@gmail.com>
2024-03-26 22:06:06 -0700

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,9 +12,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
 		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
